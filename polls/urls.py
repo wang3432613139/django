13153dfd4e3840c2424df3c6c685c340c23e4dbf
wangzelin <1362706620@qq.com>
2018-12-19 17:35:45 +0800

    提交

---
+++ polls/urls.py
@@ -0,0 +1,67 @@
+# from django.urls import path
+#
+# from . import views
+#
+# urlpatterns = [
+#     path('',views.index,name='index'),
+#     path('index',views.index,name='index')
+# ]
+
+# @app.route('/')
+# def index():
+#     pass
+
+# 先引入视图函数
+# path()函数电影1的路由最终都会在项目启动时加载
+# path（路由规则）
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    # 首页  http://ip:port/polls/
+    path('', views.index, name='index'),
+    # 首页  问题列表  polls/index/
+    # path('index', views.index, name='index'),
+    # 问题详情页 ex:/polls/l/
+    path('<int:question_id>/',views.detail,name='detail'),
+    # 投票结果页   /polls/2/results/
+    path('<int:question_id>/results', views.results, name='results'),
+    # 去投票。选项计数加一    /polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # 通过视图示例
+    path('simple/',views.SimpleView.as_view(),name='simple')
+]
+
+
+
+
+
+
+# （注意）django1.x路由写法是  正则风格
+
+# from django.conf.urls import url
+# urlpatterns[
+#     # /polls/
+#     url(r'^$',views.index,name='index'),
+#     # polls/index/
+#     url(r'^index$', views.index),
+#     # polls/5/
+#     url(r'^(?P<question_id>[0-9]+)/$'),
+# ]
+
+
+
+# 与flask对比
+# @app.route('/')
+# def index():
+#     pass
+
+
+
+
+
+# 先引入视图函数
+# path()函数定义的路由最总到会在项目启动时加载
+# path(路由规则)
---
+++ polls/views.py
@@ -0,0 +1,134 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import generic
+from .models import Question,Choice
+# Create your views here.
+# def index(request):
+#     return HttpResponse("""
+#         <html>
+#             <head>
+#             </head>
+#             <body>
+#                 <h1>hello world</h1>
+#             </body>
+#         </html>
+#     """)
+
+# def index(request):
+#
+#     """
+#     展示问题列表
+#     :return:
+#     """
+#     question_list = Question.objects.all().order_by('-pub_date')[0:5]
+#
+#     # print(question_list)
+#     # for q in question_list:
+#     #     print(q.id,q.question_text,q.pub_date)
+#     #     output=output+q.question_text
+#     # print(output)
+#
+#     # print([q.question_text for q in question_list])
+#     # output=','.join(q.question_text for q in question_list)
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'question_list':question_list
+#     }
+#     # return render_template('xx.html',q_list = q_list)
+#     return HttpResponse(template())
+
+def index(request):
+    question_list = Question.objects.order_by('-pub_date1')[:5]
+    context = {
+        'question_list': question_list
+    }
+    print(
+        1111
+    )
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+
+    """
+    显示一个问题的详细信息，问题内容、问题发布时间、选项内容、每个选项投票数
+    """
+    # try:
+    #     question = Question.objects.get(id=question_id)
+    #
+    #     # 写法1（基本思想）choices = Choice.objects.filter(question_id=question.id)
+    #     # 由于orm代劳，question直接带出对应的choices
+    #     # 写法2 choices = question.choice_set.all()
+    #     # 由于前端模板语言本质是后端代码，可以吧上句话放html页面中写，有助于降低后端复杂度
+    # except Question.DoesNotExist:
+    #     raise Http404('404,此id的问题不存在')
+    # print(question)
+    # context = {
+    #     'question':question,
+    # }
+    # 
+    # return render(request,'polls/datail.html',context)
+    try:
+        question = Question.objects.get(id=question_id)
+        print(question)
+
+        # choices = Choice.objects.filter(question=question.id)
+        # 由于orm 代劳， question 直接带出对应的choices
+        # choices = question.choice_set.all()
+        # 由于前端模板语言本质是后端代码，可以把上句话放在HTML页面中写，有助于降低后端复杂度
+    except Question.DoesNotExist:
+        raise Http404("404，此ID的问题不存在")
+    print(question)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/detail.html', context)
+    # result_set = Question.objects.all().order_by('-pub_date'[0:5])
+    #
+    #
+    # print(result_set)
+    # question = get_object_or_404(Question,id=question_id)
+    # return render(request,'polls/detail.html',{'question':question})
+def results(request,question_id):
+    """
+    投票结果
+
+    """
+    question = Question.objects.get(id=question_id)
+    return render(request,'polls/results.html',{'question':question})
+def vote(request,question_id):
+
+    """
+    投票
+    """
+    try:
+        question = Question.objects.get(id=question_id)
+        choices = question.choice_set.all()
+        choice_id = request.POST['choice']
+        selected_choice = question.choice_set.get(id=choice_id)
+    except Question.DoesNotExist as e:
+        error_message = '问题内容不存在，检查问题id'
+    except Choice.DoesNotExist as e:
+        error_message = '问题对应的选项不存在'
+        return render(request,'polls/detail.html',context={
+            'question':question,
+            'error_message':error_message
+        })
+
+    else:
+        # sql update choice se votes=votes+1 where id=2
+        selected_choice.votes +=1
+        # commit;
+        selected_choice.save()
+        # 投票完重定向到 views.resuits(qid)
+        return HttpResponseRedirect( reverse ('polls:detail',args=(question_id,)))
+    print('hello world')
+# 通过模板示例跟defindex类相比
+class SimpleView(generic.ListView):
+    template_engine = 'polls/index.html'
+    context_object_name = 'question_list'
+
+    def get_queryset(self):
+        return Question.objects.all()
